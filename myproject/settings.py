b80900eebcee2977c1adf3625481c67c9ee63ee4
macbookpro <macbookpro@mbp.local>
2020-12-20 16:05:57 +0900

    AWS S3 削除

--- myproject/settings.py
+++ myproject/settings.py
@@ -39,7 +39,6 @@ INSTALLED_APPS = [
     'myapp',
     'django.contrib.sites',
     'sitemanage',
-    'storages',
 ]
 
 SITE_ID = 1
@@ -133,18 +132,8 @@ STATIC_URL = '/static/'
 LOGIN_URL = 'myapp:login'
 LOGIN_REDIRECT_URL = 'myapp:index'
 
-
-AWS_STORAGE_BUCKET_NAME = 'djangomakeblog'
-AWS_S3_CUSTOM_DOMAIN = '%s.s3.amazonaws.com' % AWS_STORAGE_BUCKET_NAME
-AWS_S3_OBJECT_PARAMETERS = {
-    'CacheControl': 'max-age=86400', 
-}
-
-AWS_LOCATION ='media'
-DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
 MEDIA_ROOT = [os.path.join(BASE_DIR, 'media')]
-#MEDIA_URL = '/media/'
-MEDIA_URL = "https://%s/%s/" % (AWS_S3_CUSTOM_DOMAIN, AWS_LOCATION)
+MEDIA_URL = '/media/'
 
 # Heroku
 import dj_database_url
@@ -167,6 +156,3 @@ except ImportError:
 
 if not DEBUG:
     SECRET_KEY = os.environ['SECRET_KEY']
-    AWS_ACCESS_KEY_ID = os.environ['AWS_ACCESS_KEY_ID']
-    AWS_SECRET_ACCESS_KEY = os.environ['AWS_SECRET_ACCESS_KEY']
-
